b5b70aa3a6e52cfb00f653e39c9f8e3dc882889e
Stephan Schmitz <stephan.schmitz@sunzinet.com>
2018-03-12 13:02:27 +0100

    Add support for Data-Saver mode recognition

--- src/main/java/TestController.java
+++ src/main/java/TestController.java
@@ -7,10 +7,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class TestController {
 
-	/** This is used for uploading a chunk of data and test the upload speed. It does nothing, it just returns "true". */
-	@RequestMapping("/test-post")
-	public @ResponseBody String testPost() {
-		return "true";
-	}
+    /** This is used for uploading a chunk of data and test the upload speed. It does nothing, it just returns "true". */
+    @RequestMapping("/test-post")
+    public @ResponseBody String testPost() {
+    	return "true";
+    }
 
 }
